8219b2683fc8fbec087f7ba92e77346e4fb18a7f
strudels <bencurtiss6458@gmail.com>
2023-05-15 10:00:24 -0400

    Hacks necessary to load .lls file into ghidra UI
    Note that this will only load the first snapshot, and will ignore the
    other snapshots. This should be updated to have a UI popup and ask what
    snapshot should be loaded.
    
    Signed-off-by: strudels <bencurtiss6458@gmail.com>

--- process_snapshot_toolkit/ghidra/decompiler.py
+++ process_snapshot_toolkit/ghidra/decompiler.py
@@ -141,8 +141,10 @@ class GhidraDecompiler(object):
             stdout = stderr = subprocess.PIPE
             if self._verbose:
                 stdout = stderr = None
-            process = subprocess.Popen(ghidra_cmd, stdout=stdout, stderr=stderr)
-            process.communicate()
+            # process = subprocess.Popen(ghidra_cmd, stdout=stdout, stderr=stderr)
+            # process.communicate()
+            subprocess.check_call(ghidra_cmd)
+            import pdb; pdb.set_trace()
 
         finally:
             shutil.rmtree(ghidra_work_dir)
